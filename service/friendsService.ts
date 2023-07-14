e0ea686f98ad68a76aac4f43b5112d101c76f0af
Marek <wojnarmarek0@gmail.com>
2023-07-14 23:24:26 +0200

    code clean up

--- service/friendsService.ts
+++ service/friendsService.ts
@@ -1,25 +1,12 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import axiosInstance from "../helpers/axiosInstance";
 
-// export const getRecommendedPeopleAsync = createAsyncThunk(
-//     '/api/v1/User/Recomended', async ({gameId}: { gameId: string | null }) => {
-//         const data = await axiosInstance.get(`/api/v1/User/Recomended`, {gameId})
-//         console.log(data)
-//         return data.data;
-//     }
-// );
-
 export const getRecommendedPeopleAsync = createAsyncThunk(
     '/api/v1/User/Recomended', async ({gameId}: { gameId: string }) => {
         const data = await axiosInstance.get(`/api/v1/User/Recomended?gameId=${gameId}`)
-        console.log(data)
         return data.data;
     }
 );
-
-
-// 'GET' \
-//   'https://localhost:44395/api/v1/User/Recomended?gameId=c752aa65-ba0f-414f-9030-40c37692377d' \
 
 export const getFriendsListAsync = createAsyncThunk(
     '/api/v1/User/Friends', async () => {
@@ -40,7 +27,6 @@ export const addToFriendsListAsync = async (friendsIds: any) => {
     return data
 }
 export const removeFriendAsync = async (friendId: any) => {
-    // console.log(friendsIds)
     const data = await axiosInstance.delete(`/api/v1/User/Friends/${friendId}`)
     return data
 }
